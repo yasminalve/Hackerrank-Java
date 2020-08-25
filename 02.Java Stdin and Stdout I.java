14ef87684aa0a738324c1fb05b87c1804b2e8231
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-25 20:32:55 -0300

    Create 02.Java Stdin and Stdout I.java

---
+++ 02.Java Stdin and Stdout I.java
@@ -0,0 +1,18 @@
+//Yasmin Alves
+//25.08.2020
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
